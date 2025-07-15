6db77d9c04fcb557352dee84c37297a332658298
Chipmunk <198549806+Chipmunk-dev-dot@users.noreply.github.com>
2025-07-15 17:02:13 +0200

    Update ToDoList.jsx

--- todo-list/src/ToDoList.jsx
+++ todo-list/src/ToDoList.jsx
@@ -2,7 +2,22 @@ import React ,{useState} from 'react' //since we will be using react we need to 
 
 function ToDoList(){
     //STATE VARIABLES(this is how we declared variables) the setTsk and setNewTask is used to update state based on it's previous value
-    const [tasks, setTask]= useState(["Make the bed.","Do ten push-ups.","Take a cold shower","Go back to sleep..."]);
+    const [tasks, setTask]= useState([
+        {id:1,
+        name:"Make the bed.",
+        status:false
+      },
+        
+      {id:2,
+        name:"Do ten push-ups.",
+        status:false
+      },
+      
+      {id:3,
+        name:"Take a cold shower",
+        status:false
+      },
+     ]);
     const [newTask, setNewTask]= useState("");
 
     //functions we will use or need in our toto list
@@ -10,11 +25,12 @@ function ToDoList(){
         setNewTask(e.target.value);//so that we can see the text when we write into the input element
 
     }
-
+    console.log(tasks);
+ 
     function addToList(){
         //to prevent us from being able to add an empty task ,use an if statement and a trim method
         if(newTask.trim() !==""){ //trim function to reemove an whitespaces
-            setTask(t =>[...t, newTask]); //add new tasks to the existing ones (the ...spread operator allows us to copy parts of an existing array to a new one)
+            setTask(t =>[...t, {id:tasks.length+1,name:newTask,status:false}]); //add new tasks to the existing ones (the ...spread operator allows us to copy parts of an existing array to a new one)
             setNewTask(""); //???what does it do
 
         }
@@ -68,7 +84,7 @@ function ToDoList(){
         {tasks.map((task, index)=>
             <li key={index}> {/* jdfdkjdk */}
             <input type='checkbox'/>
-            <span  className='text'>{task}</span>
+            <span  className='text'>{task.name}</span>
             <button className='delete-button' onClick={() => removeFromList(index)}>🗑</button>
             <button className='up-button' onClick={() => moveUp(index)}>Up</button>
             <button className='down-button' onClick={() => moveDown(index)}>Down</button>
