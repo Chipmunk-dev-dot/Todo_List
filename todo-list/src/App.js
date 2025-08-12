0a110422bd26817fe6b7379edb7abfdab625f5e4
Chipmunk <198549806+Chipmunk-dev-dot@users.noreply.github.com>
2025-08-12 14:53:09 +0200

    Update App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -10,19 +10,3 @@ function App() {
 }
 
 export default App;
-/* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I'ts not a goodmorning ...
-        </a>
-      </header>
-    </div>*/
