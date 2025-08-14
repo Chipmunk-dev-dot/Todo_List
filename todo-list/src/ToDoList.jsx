96a6ec8973af89907b1766445b2871727e9a14ce
Chipmunk <198549806+Chipmunk-dev-dot@users.noreply.github.com>
2025-08-14 14:08:34 +0200

    Update ToDoList.jsx

--- todo-list/src/ToDoList.jsx
+++ todo-list/src/ToDoList.jsx
@@ -1,55 +1,73 @@
-import React, { useEffect, useState } from "react"; //since we will be using react we need to import it and then specify the hook used...
+import React, { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
+import EditDialog from "./components/EditDialog";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 function ToDoList() {
+  const [newTask, setNewTask] = useState("");
+  const queryClient = useQueryClient();
 
-  const [tasks, setTasks]= useState([]);
-  const [newTask, setNewTask]=useState("");
+  const { isPending, error, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: () => axios("http://localhost:5001/api/v1/todos"),
+  });
 
-  useEffect(() => {
+  const createData = useMutation({
+    mutationFn: ( newTask ) => {
+      return axios.post("http://localhost:5001/api/v1/todos", {
+        task_name:newTask,
+        status:'in Progress'
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(data);
+    },
+  });
 
-    const getData = async () => {
-      try {
-        const response = await fetch("http://localhost:5001/api/v1/todos", {
-          method: "GET",
-        });
+  const deleteData=useMutation({
+    mutationFn: (id)=>{
+      return axios.delete(`http://localhost:5001/api/v1/todos/${id}`,
+        id
+      )
+    },
+    onSuccess:()=>{
+      queryClient.invalidateQueries(data);
+    }
+  });
 
-        const data = await response.json();
-        console.log(data); // handle or set data to state here
-        setTasks(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    getData();
-  }, []);
+  const updateData = useMutation({
+    mutationFn: ({id,task})=>{
+      return axios.put(`http://localhost:5001/api/v1/todos/${id}`,{
+
+        task_name:task
+      })
+
+    },
+      onSuccess:()=>{
+      queryClient.invalidateQueries(data);
+    }
+
+  })
 
 
-const createData=async()=>{
-  try {
-    const response= await fetch("http://localhost:5001/api/v1/todos", {
-      method: "POST",
-      body: JSON.stringify({
-        "task_name": newTask,
-        "status": "in-progress"
-         }),
-  headers: {
-    "Content-type": "application/json"
+
+  const createTask = () => {
+    createData.mutate(newTask);
+    setNewTask("");
+  };
+
+  const deleteTask =(id)=>{
+    deleteData.mutate(id);
   }
-});
-    
- const data=await response.json();
- console.log(data)
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    
-  }
-  
-
-}
- 
 
 
-  
+  console.log(data?.data);
+
+  const myTasks = data?.data;
+
   return (
     <>
       <div className="todo-list">
@@ -57,23 +75,43 @@ const createData=async()=>{
       </div>
 
       <label>
-        <input  type="text" value={newTask} placeholder="Enter a task..." onChange={(e)=> setNewTask(e.target.value   )
-    }></input>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        ></input>
       </label>
-      <button className="add-button"  onClick={()=> createData()}>+</button>
+      <button className="add-button" onClick={() => createTask()}>
+        +
+      </button>
       <div className="big-box">
         <div className="list-box">
           <ol>
-             {tasks?.map(//to check if the map is defined or not
-              (task,index ) => (
-                <li key={index}>{task.task_name}
-                <button className="delete-button">Delete</button>
-                
+            {myTasks?.map(
+              //to check if the map is defined or not
+              (task, index) => (
+                <li key={index}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      height: "60px",
+                      justifyContent: "space-between",
+                      width: "100%",
+                    }}
+                  >
+                    <h5>{task.task_name}</h5>
+
+                    <div>
+                      <EditDialog  id= {task.id} updateData={updateData}/>
+                      <button onClick={()=>deleteTask(task.id)}> Delete</button>
+                    </div>
+                  </div>
                 </li>
               )
             )}
           </ol>
-          <dialog> Edit</dialog>
         </div>
       </div>
     </>
